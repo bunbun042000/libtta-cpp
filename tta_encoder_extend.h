decfb94e377d077c15ba32963d207fc7d0025fe3
Yamagata Fumihiro <bunbun042000@gmail.com>
2025-12-16 17:02:08 +0900

    Add include guard instead of #pragma once.

--- tta_encoder_extend.h
+++ tta_encoder_extend.h
@@ -17,7 +17,9 @@ License along with this library; if not, write to the Free Software
 Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
 */
 
-#pragma once
+#ifndef TTA_ENCODER_EXTEND_H_INCLUDED
+#define TTA_ENCODER_EXTEND_H_INCLUDED
+
 #include "libtta.h"
 
 static const TTAuint64 MAX_SAMPLES = 4294967295;
@@ -40,3 +42,5 @@ namespace tta
 	};
 
 }
+
+#endif // #ifndef TTA_ENCODER_EXTEND_H_INCLUDED
